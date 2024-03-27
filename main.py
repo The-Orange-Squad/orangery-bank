3b4c9409a9c878b35349e961750b971119cd9910
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-03-27 13:12:25 -0400

    Add basic Discord bot functionality

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from dotenv import load_dotenv
 import os
 from management.shop import Shop
 from data.user import User
+import discord
+from discord.ext import commands
+from discord.commands import Option
 
 shop = Shop()
 shop.process()
 
-print(shop.get_processed())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot.remove_command("help")
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
+bot.run(TOKEN)
