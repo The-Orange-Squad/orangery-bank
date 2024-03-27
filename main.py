e2377afeb9f0c5f1f5c4353d064f91d1b615acb0
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-03-27 13:53:17 -0400

    Add Discord UI components and implement a shop view

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from linker import linker
 import discord
 from discord.ext import commands
 from discord.commands import Option
+from discord.ui import Button, Select, View
 
 
 load_dotenv()
@@ -29,6 +30,7 @@ async def on_ready():
     
 @bot.slash_command(name="balance", description="Check the balance of the specified user")
 async def balance(ctx, user: Option(User, "The user to check the balance of", required=True)):
+    await ctx.defer()
     user_ = User()
     user_.load(user.id)
     if linker.needicon:
@@ -39,6 +41,72 @@ async def balance(ctx, user: Option(User, "The user to check the balance of", re
                 emoji = str(moji)
     embed = discord.Embed(title=f"{user.name}'s balance", description=f"Balance: {user_.balance} {emoji if linker.needicon else ''}")
     await ctx.respond(embed=embed)
+
+class ShopLRView(View):
+    def __init__(self, ctx, embed, list_):
+        super().__init__(timeout=None)
+        self.ctx = ctx
+        self.embed = embed
+        self.list = list_
+        self.page = 0
+        keys_list = list(list_.keys())
+        self.pages = [{key: list_[key] for key in keys_list[i:i+5]} for i in range(0, len(keys_list), 5)]  # Convert dictionary into a list of sub-dictionaries
+        user = User()
+        user.load(ctx.author.id)
+        self.user = user
+        self.inventory = user.inventory
+
+    async def pre_rendder(self):
+        self.embed.clear_fields()
+        for item in self.pages[self.page]:
+            if item in self.inventory:
+                addedtext = f" (You already own {self.inventory[item]})"
+            else:
+                addedtext = ""
+            self.embed.add_field(name=f"{item} | Price: {self.list[item]['price']}", value=self.list[item]['desc'] + addedtext, inline=False)
+        return self.embed
+    
+    # Adds ⬅️ Left and Right ➡️ buttons to the view, all items are split into pages of 5
+    @discord.ui.button(label="⬅️", style=discord.ButtonStyle.primary)
+    async def left(self, button: discord.ui.Button, interaction: discord.Interaction):
+        # Each item is shown in a field rendered as "name | price: price" with the description as the value
+        self.page -= 1
+        if self.page < 0:
+            self.page = len(self.pages) - 1
+        self.embed.clear_fields()
+        for item in self.pages[self.page]:
+            if item in self.inventory:
+                addedtext = f" (You already own {self.inventory[item]})"
+            else:
+                addedtext = ""
+            self.embed.add_field(name=f"{item} | Price: {self.list[item]['price']}", value=self.list[item]['desc'] + addedtext, inline=False)
+        await interaction.response.edit_message(embed=self.embed, view=self)
+
+    @discord.ui.button(label="➡️", style=discord.ButtonStyle.primary)
+    async def right(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.page += 1
+        if self.page >= len(self.pages):
+            self.page = 0
+        self.embed.clear_fields()
+        for item in self.pages[self.page]:
+            if item in self.inventory:
+                addedtext = f" (You already own {self.inventory[item]})"
+            else:
+                addedtext = ""
+            self.embed.add_field(name=f"{item} | Price: {self.list[item]['price']}", value=self.list[item]['desc'] + addedtext, inline=False)
+        await interaction.response.edit_message(embed=self.embed, view=self)
+
+
+    
+
+@bot.slash_command(name="shop", description="View the shop")
+async def shop_(ctx):
+    await ctx.defer()
+    embed = discord.Embed(title="Shop", description="Here's what's in the shop!")
+    itemlist = shop.get_raw()
+    lrv = ShopLRView(ctx, embed, itemlist)
+    await lrv.pre_rendder()
+    await ctx.respond(embed=embed, view=lrv)
 
 
 bot.run(TOKEN)
