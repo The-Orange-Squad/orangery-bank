a28a392df4323862d48f7f42327fb98db6f16725
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-03-27 07:36:31 -0400

    Refactor User class and add file handling for user data

--- data/user.py
+++ data/user.py
@@ -2,7 +2,7 @@ import json
 import os
 
 basexpreq = 100
-basexpreqmod = 1.5 # only applied at lvl 2+
+basexpreqmod = 1.5  # only applied at lvl 2+
 
 class User:
     def __init__(self):
@@ -15,50 +15,43 @@ class User:
         self.lvl = 0
         self.xp = 0
         self.altids = []
-    
-    def exists(self, id):
-        with open(f"data/users/basehandler.json", "r") as f:
-            data = json.load(f)
-            return str(id) in data
 
     def save(self):
-        with open(f"data/users/basehandler.json", "w") as f:
+        with open(f"data/users/{self.id}.json", "w") as f:
             json.dump(self.__dict__, f)
     
-    def load(self, id, returner=False):
-        with open(f"data/users/basehandler.json", "r") as f:
-            data = json.load(f)
-            self.id = id
-            conv = {}
-            # to int
-            for key in data:
-                conv[key] = int(data[key])
-            
-            data = conv
 
-            try:
+
+    def load(self, id, returner=False):
+        self.id = id
+        filename = f"data/users/{id}.json"
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                data = json.load(f)
+                self.balance = data.get("balance", 0)
+                self.inventory = data.get("inventory", {})
+                self.msgc = data.get("msgc", 0)
+                self.banned = data.get("banned", False)
+                self.pos = data.get("pos", "norm")
+                self.lvl = data.get("lvl", 0)
+                self.xp = data.get("xp", 0)
+                self.altids = data.get("altids", [])
                 if returner:
-                    causeerror = 1 / 0
-                self.balance = data[id]["balance"]
-                self.inventory = data[id]["inventory"]
-                self.msgc = data[id]["msgc"]
-                self.banned = data[id]["banned"]
-                self.pos = data[id]["pos"]
-                self.lvl = data[id]["lvl"]
-                self.xp = data[id]["xp"]
-                self.altids = data[id]["altids"]
-            except:
-                pass
-            
+                    return self
+                return False
+        else:
             if returner:
                 return self
-            
             return False
-    
+
+    def exists(self, id):
+        filename = f"data/users/{id}.json"
+        return os.path.exists(filename)
+
     def ban(self):
         self.banned = True
         self.save()
-    
+
     def give_xp(self, amount):
         self.xp += amount
         # if lvl 0 -> 1, don't apply the mod
@@ -67,18 +60,23 @@ class User:
         elif self.lvl > 0 and self.xp >= basexpreq * (basexpreqmod ** self.lvl):
             self.lvl += 1
         self.save()
-    
+
     def set_alt(self, id):
         self.altids.append(id)
         self.save()
-    
+
     def isaltof(self, id):
         return id in self.altids
-    
+
     def syncwith(self, id_):
         targetdata = self.load(id_, True)
         mydata = self.__dict__
 
         for key in targetdata.__dict__:
             mydata[key] = targetdata.__dict__[key]
-        
+
+        self.save()
+    
+    def erase(self):
+        os.remove(f"data/users/{self.id}.json")
+        return True
